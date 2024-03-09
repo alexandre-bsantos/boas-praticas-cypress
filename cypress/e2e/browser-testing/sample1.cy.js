a5ad8390ef190e91ccdd9d641fe3ea3111bba267
alexandre-bsantos <alexandre_bsantos12@hotmail.com>
2024-03-09 15:01:31 -0300

    Erro em browser testing sample 1

--- cypress/e2e/browser-testing/sample1.cy.js
+++ cypress/e2e/browser-testing/sample1.cy.js
@@ -4,8 +4,7 @@ describe('Browser testing bad practice - anchor href', () => {
   })
 
   it('directs the user to the login page when clicking the login link', () => {
-    cy.contains('.nav a', 'Login').click()
+    cy.contains('.nav a', 'Login').should('not.have.attr', 'target').and('have.attr', 'href', '/login')
 
-    cy.url().should('be.equal', 'https://notes-serverless-app.com/login')
   })
 })
